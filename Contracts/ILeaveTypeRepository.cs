8268c3699b7d2b30ed745e82dcca3acd16923c19
elege010725 <elege01@gmail.com>
2020-10-17 23:24:48 -0400

    Add project files.

---
+++ Contracts/ILeaveTypeRepository.cs
@@ -0,0 +1,15 @@
+﻿using Leave_Management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Contracts
+{
+    public interface ILeaveTypeRepository : IRepositoryBase<LeaveType>
+    {
+
+        ICollection<LeaveType> GetEmployeesByLeaveType(int id);
+
+    }
+}
---
+++ Repository/LeaveAllocationRepository.cs
@@ -0,0 +1,57 @@
+﻿using Leave_Management.Contracts;
+using Leave_Management.Data;
+using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Leave_Management.Repository
+{
+    public class LeaveAllocationRepository : ILeaveAllocationRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveAllocationRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public bool Create(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveAllocation> FindAll()
+        {
+            var LeaveAllocations = _db.LeaveAllocations.ToList();
+            return LeaveAllocations;
+        }
+
+        public LeaveAllocation FindById(int id)
+        {
+            var LeaveAllocation = _db.LeaveAllocations.Find(id);
+            return LeaveAllocation;
+        }
+    
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveAllocation entity)
+        {
+            _db.LeaveAllocations.Update(entity);
+            return Save();
+        }
+    }
+}
